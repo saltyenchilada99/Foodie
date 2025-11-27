7aedc43ddd4e398d45cfbc4698514fe28afe1f68
saltyenchilada99 <matthew_weber03@yahoo.com>
2025-11-26 23:26:37 -0500

    Test link

--- foodie-server.js
+++ foodie-server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const pool = require("./database");
 
 app.get("/", (req, res) => {
   res.send("Hello from cloud!");
